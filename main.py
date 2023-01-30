5066681d32223822230e9dbd643ca186820cde7d
cray.j <cray.j@kidsnote.com>
2023-01-30 12:32:04 +0900

    add sentry

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn='https://e1becb71961e4be8893a9a2a8c847281@o4504562589564928.ingest.sentry.io/4504591390801920',
+    traces_sample_rate=1.0
+)
+
+
 def main():
     print('main')
 
